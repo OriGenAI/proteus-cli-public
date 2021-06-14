66c0c847662c59e8d40af035a9167469606d6cee
Eduardo <eduardo@origen.ai>
2021-06-14 20:12:51 +0200

    better nicer code

--- simulations.py
+++ simulations.py
@@ -32,23 +32,30 @@ def upload_file_to_batch(url, source_path, filepath):
         return response_json.get('case')
 
 
+def find_data_files(source_folder):
+    for root, dirs, files in os.walk(source_folder):
+        for file_ in files:
+            if file_.endswith(".DATA"):
+                yield os.path.join(root, file_)
+
+
+def provide_case_depedencies(simulations_batch_url, dependencies, source_folder):
+    for dependency_item in dependencies:
+        filepath = dependency_item.get('path')
+        source_path = f"{source_folder}/{filepath}"
+        upload_file_to_batch(simulations_batch_url, source_path, filepath)
+
+
 def upload_to_batch(source_folder, batch_uuid):
     simulations_batch_url = f"api/v1/simulations/{batch_uuid}"
     response = api.get(simulations_batch_url)
     assert response.status_code == 200, f"Expectend batch to be created but got {response.content}"
     assert 'simulation_batch' in response.json()
     batch = response.json().get("simulation_batch")
-    for root, dirs, files in os.walk(source_folder):
-        print('root', root, 'dirs', dirs)
-        for file_ in files:
-            if file_.endswith(".DATA"):
-                source_path = os.path.join(root, file_)
-                filepath = source_path.replace(source_folder + '/', '')
-                case = upload_file_to_batch(simulations_batch_url, source_path, filepath)
-                assert 'dependencies' in case
-                dependencies = case.get('dependencies')
-                print(f"{filepath} has {len(dependencies)} pending dependencies")
-                for dependency_item in dependencies:
-                    filepath = dependency_item.get('path')
-                    source_path = f"{source_folder}/{filepath}"
-                    upload_file_to_batch(simulations_batch_url, source_path, filepath)
+    for source_path in find_data_files(source_folder):
+        filepath = source_path.replace(f"{source_folder}/", "")
+        case = upload_file_to_batch(simulations_batch_url, source_path, filepath)
+        assert 'dependencies' in case
+        dependencies = case.get('dependencies')
+        print(f"{filepath} has {len(dependencies)} pending dependencies")
+        provide_case_depedencies(simulations_batch_url, dependencies, source_folder)
