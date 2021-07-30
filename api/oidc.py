88b86684ec340e06628a60a37d1c8d31bf0efa86
Eduardo <eduardo@origen.ai>
2021-07-30 07:49:25 +0200

    more robust mechanism to update token

--- api/oidc.py
+++ api/oidc.py
@@ -1,9 +1,11 @@
 import requests
 from config import config
-import base64
-import json
-from threading import Timer
+from threading import Timer, Lock
 import certifi
+import json
+import base64
+from functools import wraps
+import time
 
 (
     AUTH_HOST,
@@ -30,6 +32,27 @@ class RepeatTimer(Timer):
             self.function(*self.args, **self.kwargs)
 
 
+def may_insist_up_to(times, delay_in_secs=0):
+    def wil_retry_if_fails(fn):
+        @wraps(fn)
+        def wrapped(*args, **kwargs):
+            failures = 0
+            while failures < times:
+                try:
+                    return fn(*args, **kwargs)
+                except Exception as error:
+                    failures += 1
+                    if failures > times:
+                        raise error
+                    else:
+                        print("+", end="")
+                        time.sleep(delay_in_secs)
+
+        return wrapped
+
+    return wil_retry_if_fails
+
+
 class OIDC:
     def __init__(
         self,
@@ -44,6 +67,7 @@ class OIDC:
         self.realm = realm
         self.client_id = client_id
         self.client_secret = client_secret
+        self._access_token_locked = Lock()
         self._last_res = None
         self._refresh_timer = None
         self._when_login_callback = None
@@ -64,14 +88,17 @@ class OIDC:
         self._resfresh_expires_in = refresh_expires_in
 
     @property
-    def access_token_parsed(self):
-        _header, payload, _sig = self.access_token.split('.')
-        payload = payload + "=" * divmod(len(payload), 4)[1]
-        return json.loads(base64.urlsafe_b64decode(payload))
+    def access_token(self):
+        self._access_token_locked.acquire()
+        result = self._access_token
+        self._access_token_locked.release()
+        return result
 
     @property
-    def access_token(self):
-        return self._access_token
+    def access_token_parsed(self):
+        _header, payload, _sig = self.access_token.split(".")
+        payload = payload + "=" * divmod(len(payload), 4)[1]
+        return json.loads(base64.urlsafe_b64decode(payload))
 
     @property
     def refresh_token(self):
@@ -99,6 +126,20 @@ class OIDC:
     def when_refresh(self, callback):
         self._when_refresh_callback = callback
 
+    @may_insist_up_to(3, delay_in_secs=1)
+    def send_login_request(self, login):
+        response = requests.post(
+            self.url,
+            data=login,
+            verify=certifi.where(),
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+        if response.status_code == 401:
+            # No need to be blunt
+            return None
+        response.raise_for_status()
+        return response
+
     def do_login(self, password=PASSWORD, username=None, auto_update=True):
         login = {
             "grant_type": "password",
@@ -108,12 +149,7 @@ class OIDC:
         }
         if self.client_secret is not None:
             login["client_secret"] = self.client_secret
-        response = requests.post(
-            self.url,
-            data=login,
-            verify=certifi.where(),
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-        )
+        response = self.send_login_request(login)
 
         credentials = response.json()
         assert "access_token" in credentials
@@ -134,8 +170,21 @@ class OIDC:
         )
         self._refresh_timer.start()
 
+    @may_insist_up_to(5, delay_in_secs=1)
+    def send_refresh_request(self, refresh):
+        response = requests.post(
+            self.url,
+            data=refresh,
+            verify=certifi.where(),
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+        response.raise_for_status()
+        return response
+
     def do_refresh(self):
         assert self.refresh_token is not None
+        print("Performing token update.", end="")
+        self._access_token_locked.acquire()
         refresh = {
             "grant_type": "refresh_token",
             "refresh_token": self.refresh_token,
@@ -143,17 +192,17 @@ class OIDC:
         }
         if self.client_secret is not None:
             refresh["client_secret"] = self.client_secret
-        response = requests.post(
-            self.url,
-            data=refresh,
-            verify=certifi.where(),
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-        )
-        if response.status_code != 200:
+        try:
+            response = self.send_refresh_request(refresh)
+            credentials = response.json()
+            assert credentials.get("access_token") is not None
+            self._update_credentials(**credentials)
+            print(" Done.")
+        except Exception:
+            print(" Failed.")
             return self.do_login()
-        credentials = response.json()
-        assert credentials.get("access_token") is not None
-        self._update_credentials(**credentials)
+        finally:
+            self._access_token_locked.release()
         if self._when_refresh_callback is not None:
             self._when_refresh_callback()
 
