42ef96b59a6a8dd589ea2e92700d35b37246fff9
Michael Green <47226537+mcgreen-origen@users.noreply.github.com>
2021-09-17 04:18:16 -0400

    added requirement automatic readin (#10)
    Co-authored-by: Eduardo Orive Vinuesa <eduardo@origen.ai>

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 import project
+import distutils
+import os
+with open("requirements/prod.txt") as f:
+    requirements = f.read().splitlines()
+
 
 
 setup(
     name=project.name,
     version=project.version,
     py_modules=["proteus"],
-    install_requires=[
-        "Click",
-    ],
+    install_requires=requirements,
     entry_points="""
         [console_scripts]
         proteus=proteus:main
