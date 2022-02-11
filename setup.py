1a1fe4a67e4ae0da3cef44df7902214f86d14efb
Eduardo <eduardo@origen.ai>
2022-02-11 10:43:10 +0100

    filtered out the local submodule

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ from setuptools import setup, find_packages
 import project
 
 
-with open("requirements/prod.txt") as f:
-    requirements = f.read().splitlines()
+def translate_local(source):
+    print("source", source)
+    if "==" in source:
+        return source
+    return ""
 
+
+with open("requirements/prod.txt") as f:
+    sources = f.read().splitlines()
+    requirements = map(translate_local, sources)
 
 setup(
     name=project.name,
