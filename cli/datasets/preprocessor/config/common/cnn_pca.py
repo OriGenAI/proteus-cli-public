15b45e3550ed1474aaffc8cd050691fd9800c592
Luis Arce <luis@origen.ai>
2023-02-17 11:41:15 +0100

    ~ Small fix

--- cli/datasets/preprocessor/config/common/cnn_pca.py
+++ cli/datasets/preprocessor/config/common/cnn_pca.py
@@ -5,50 +5,50 @@ from ...utils import RequiredFilePath
 class CnnPcaCommonConfig(DefaultConfig):
     """Configuration generator for the common files"""
 
-    # def step_1_runspec(self):
-    #     """
-    #     List all cases and its steps to generate the .DATA iterator
-    #
-    #     Args: -
-    #
-    #     Returns:
-    #         iterator: the list of steps to preprocess
-    #     """
-    #     first_case = self.cases[0]
-    #     return iter(
-    #         [
-    #             {
-    #                 "input": [RequiredFilePath(f'{first_case["root"].rstrip("/")}' + f'/*.DATA', download_name="data")],
-    #                 "output": ["runspec.p"],
-    #                 "preprocessing": "export_runspec",
-    #                 "case": first_case["number"],
-    #                 "keep": True,
-    #                 "additional_info": {"set_endpoint": self._set_endpoint},
-    #             }
-    #         ]
-    #     )
-    #
-    # def step_2_wellspec(self):
-    #     """
-    #     List all cases and its steps to generate the Summaries iterator
-    #
-    #     Args: -
-    #
-    #     Returns:
-    #         iterator: the list of steps to preprocess
-    #     """
-    #     first_case = self.cases[0]
-    #     return iter(
-    #         [
-    #             {
-    #                 "input": [RequiredFilePath(f'{first_case["root"].rstrip("/")}' + f'/*.DATA', download_name="data")],
-    #                 "output": ["well_spec.p"],
-    #                 "preprocessing": "export_wellspec",
-    #                 "keep": True,
-    #                 "case": first_case["number"],
-    #             }
-    #         ]
-    #     )
+    def step_1_runspec(self):
+        """
+        List all cases and its steps to generate the .DATA iterator
+
+        Args: -
+
+        Returns:
+            iterator: the list of steps to preprocess
+        """
+        first_case = self.cases[0]
+        return iter(
+            [
+                {
+                    "input": [RequiredFilePath(f'{first_case["root"].rstrip("/")}' + f'/*.DATA', download_name="data")],
+                    "output": ["runspec.p"],
+                    "preprocessing": "export_runspec",
+                    "case": first_case["number"],
+                    "keep": True,
+                    "additional_info": {"set_endpoint": self._set_endpoint},
+                }
+            ]
+        )
+
+    def step_2_wellspec(self):
+        """
+        List all cases and its steps to generate the Summaries iterator
+
+        Args: -
+
+        Returns:
+            iterator: the list of steps to preprocess
+        """
+        first_case = self.cases[0]
+        return iter(
+            [
+                {
+                    "input": [RequiredFilePath(f'{first_case["root"].rstrip("/")}' + f'/*.DATA', download_name="data")],
+                    "output": ["well_spec.p"],
+                    "preprocessing": "export_wellspec",
+                    "keep": True,
+                    "case": first_case["number"],
+                }
+            ]
+        )
 
     def step_3_dat_files(self):
         """
