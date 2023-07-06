17466ce2bdfc1be9752949b069019faa04392af7
Luis Arce <luis@origen.ai>
2023-07-06 18:37:27 +0200

    PS-482 ~ Finished ~ Updated dependencies ~ Small fix

--- cli/datasets/preprocessor/preprocess_functions.py
+++ cli/datasets/preprocessor/preprocess_functions.py
@@ -280,7 +280,7 @@ def export_dat_properties(output_source: LocalSource, input_files: Sequence[Path
     with EXPORT_DAT_PROPERTIES_LOCK:
         case_dest_loc = []
         # Dat files can be very big. Write them individually to reduce memory usage
-        for source, col_name, df in dat.preprocess(dat_src_locs, do_yield=True):
+        for source, col_name, df in dat.preprocess(dat_src_locs):
             case_dest_loc.append(_write_keywords_to_h5({col_name: source}, output_source.uri)[0])
 
     return dat_src_locs, case_dest_loc, None
